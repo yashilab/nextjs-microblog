2279cc349b6cded6b9ddabf38262608a0635cd96
yashi <y.toyama@yashilab.com>
2022-08-04 13:06:43 +0900

    all done

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import utilStyles from "../../styles/utils.module.css";
+
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+                <title>{`${postData.title} | ${siteTitle}`}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>{postData.date}</div>
+                <div dangerouslySetInnerHTML={{ __html: postData.blogContentHtml }} />
+            </article>
+        </Layout>
+    );
+}
